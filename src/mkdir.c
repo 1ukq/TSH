7c09dc4d294da1c321a4ea22c671f2e40aacb190
Luka <luka.laval@etu.univ-paris-diderot.fr>
2020-10-22 19:32:19 +0200

    écriture EXACTE v00

--- src/mkdir.c
+++ src/mkdir.c
@@ -40,22 +40,16 @@ int makedir(char * path_tar, char * path_cwd, char * dir_name)
 			{
 				p.name[i] = '\0';
 			}
-			for(i = 0; i < sizeof(p.name); i++)
-			{
-				entete[i] = p.name[i];
-			}
 			printf("p.name : %s\n", p.name);
 
 			/* p.mode */
-			sprintf(p.mode, "%s", "0000755");
-			for(i = 0; i < sizeof(p.mode); i++)
-			{
-				entete[100 + i] = p.mode[i];
-			}
+			sprintf(p.mode, "%s", "000755");
+			p.mode[sizeof(p.mode)-2] = ' ';
+			p.mode[sizeof(p.mode)-1] = '\0';
 			printf("p.mode : %s\n", p.mode);
 
 			/* uid */
-			sprintf(p.uid, "%7o", getuid());
+			sprintf(p.uid, "%6o", getuid());
 			for(i = 0; i < sizeof(p.uid)-1; i++)
 			{
 				if(p.uid[i] == ' ')
@@ -63,15 +57,12 @@ int makedir(char * path_tar, char * path_cwd, char * dir_name)
 					p.uid[i] = '0';
 				}
 			}
+			p.uid[sizeof(p.uid)-2] = ' ';
 			p.uid[sizeof(p.uid)-1] = '\0';
-			for(i = 0; i < sizeof(p.uid); i++)
-			{
-				entete[108 + i] = p.uid[i];
-			}
 			printf("p.uid : %s\n", p.uid);
 
 			/* gid */
-			sprintf(p.gid, "%7o", getgid());
+			sprintf(p.gid, "%6o", getgid());
 			for(i = 0; i < sizeof(p.gid); i++)
 			{
 				if(p.gid[i] == ' ')
@@ -79,11 +70,8 @@ int makedir(char * path_tar, char * path_cwd, char * dir_name)
 					p.gid[i] = '0';
 				}
 			}
+			p.gid[sizeof(p.gid)-2] = ' ';
 			p.gid[sizeof(p.gid)-1] = '\0';
-			for(i = 0; i < sizeof(p.gid); i++)
-			{
-				entete[116 + i] = p.gid[i];
-			}
 			printf("p.gid : %s\n", p.gid);
 
 			/* size */
@@ -91,32 +79,20 @@ int makedir(char * path_tar, char * path_cwd, char * dir_name)
 			{
 				p.size[i] = '0';
 			}
-			p.size[sizeof(p.size)-1] = '\0';
-			for(i = 0; i < sizeof(p.size); i++)
-			{
-				entete[124 + i] = p.size[i];
-			}
+			p.size[sizeof(p.size)-1] = ' ';
 			printf("p.size : %s\n", p.size);
 
 			/* mtime */
-			sprintf(p.mtime, "%li", time(NULL));
-			for(i = 0; i < sizeof(p.mtime); i++)
-			{
-				entete[136 + i] = p.mtime[i];
-			}
+			sprintf(p.mtime, "%lo", time(NULL));
+			p.mtime[sizeof(p.mtime)-1] = ' ';
 			printf("p.mtime : %s\n", p.mtime);
 
 			/* chksum */
 			set_checksum(&p);
-			for(i = 0; i < sizeof(p.chksum); i++)
-			{
-				entete[148 + i] = p.chksum[i];
-			}
 			printf("p.chksum : %s\n", p.chksum);
 
 			/* typeflag */
 			p.typeflag = '5';
-			entete[156] = p.typeflag;
 			printf("p.typeflag : %c\n", p.typeflag);
 
 			/* linkname */
@@ -124,26 +100,14 @@ int makedir(char * path_tar, char * path_cwd, char * dir_name)
 			{
 				p.linkname[i] = '\0';
 			}
-			for(i = 0; i < sizeof(p.linkname); i++)
-			{
-				entete[157 + i] = p.linkname[i];
-			}
 			printf("p.linkname : %s\n", p.linkname);
 
 			/* magic */
 			sprintf(p.magic, "ustar");
-			for(i = 0; i < sizeof(p.magic); i++)
-			{
-				entete[257 + i] = p.magic[i];
-			}
 			printf("p.magic : %s\n", p.magic);
 
 			/* version */
 			sprintf(p.version, "00");
-			for(i = 0; i < sizeof(p.version); i++)
-			{
-				entete[263 + i] = p.version[i];
-			}
 			printf("p.version : %s\n", p.version);
 
 			/* uname */
@@ -152,10 +116,6 @@ int makedir(char * path_tar, char * path_cwd, char * dir_name)
 			for(i = strlen(p.uname); i < sizeof(p.uname); i++)
 			{
 				p.uname[i] = '\0';
-			}
-			for(i = 0; i < sizeof(p.uname); i++)
-			{
-				entete[265 + i] = p.uname[i];
 			}
 			printf("p.uname : %s\n", p.uname);
 
@@ -166,10 +126,6 @@ int makedir(char * path_tar, char * path_cwd, char * dir_name)
 			{
 				p.gname[i] = '\0';
 			}
-			for(i = 0; i < sizeof(p.gname); i++)
-			{
-				entete[297 + i] = p.gname[i];
-			}
 			printf("p.gname : %s\n", p.gname);
 
 			/* devmajor */
@@ -179,10 +135,6 @@ int makedir(char * path_tar, char * path_cwd, char * dir_name)
 			}
 			p.devmajor[sizeof(p.devmajor)-2] = ' ';
 			p.devmajor[sizeof(p.devmajor)-1] = '\0';
-			for(i = 0; i < sizeof(p.devmajor); i++)
-			{
-				entete[329 + i] = p.devmajor[i];
-			}
 			printf("p.devmajor : %s\n", p.devmajor);
 
 			/* devminor */
@@ -192,20 +144,12 @@ int makedir(char * path_tar, char * path_cwd, char * dir_name)
 			}
 			p.devminor[sizeof(p.devminor)-2] = ' ';
 			p.devminor[sizeof(p.devminor)-1] = '\0';
-			for(i = 0; i < sizeof(p.devminor); i++)
-			{
-				entete[337 + i] = p.devminor[i];
-			}
 			printf("p.devminor : %s\n", p.devminor);
 
 			/* prefix */
 			for(i = 0; i < sizeof(p.prefix); i++)
 			{
 				p.prefix[i] = '\0';
-			}
-			for(i = 0; i < sizeof(p.prefix); i++)
-			{
-				entete[345 + i] = p.prefix[i];
 			}
 			printf("p.prefix : %s\n", p.prefix);
 
@@ -214,22 +158,19 @@ int makedir(char * path_tar, char * path_cwd, char * dir_name)
 			{
 				p.junk[i] = '\0';
 			}
-			for(i = 0; i < sizeof(p.junk); i++)
-			{
-				entete[500 + i] = p.junk[i];
-			}
 			printf("p.junk : %s\n", p.junk);
 
 
 			// puis on ecrit tout dans l'ordre et la taille indiqué dans le posix_header en completant avec des '\0'
 
 			n = lseek(fd, (-1)*BLOCK_SIZE, SEEK_CUR);
+			printf("%i\n", SEEK_CUR);
 			if(n < 0)
 			{
 				perror("lseek 2");
 				return -1;
 			}
-			n = write(fd, entete, BLOCK_SIZE);
+			n = write(fd, &p, BLOCK_SIZE);
 			if(n < 0)
 			{
 				perror("write");
@@ -239,6 +180,7 @@ int makedir(char * path_tar, char * path_cwd, char * dir_name)
 			char end_block[BLOCK_SIZE];
 			memset(end_block, '\0', BLOCK_SIZE);
 
+			n = write(fd, end_block, BLOCK_SIZE);
 			n = write(fd, end_block, BLOCK_SIZE);
 
 			close(fd);
