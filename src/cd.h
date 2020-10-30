925243543846d7fd352412e4e0b4700e36eb2707
Luka <luka.laval@etu.univ-paris-diderot.fr>
2020-10-30 13:33:00 +0100

    cd.c

--- src/cd.h
+++ src/cd.h
@@ -1,3 +1,6 @@
+#ifndef CD
+#define CD
+
 #include <string.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -5,5 +8,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include "types/posix_header.h"
+#include "types/work_directory.h"
 
 #define BLOCK_SIZE 512
+
+int cd(struct work_directory * ad_cwd, struct work_directory * ad_nwd);
+
+#endif
