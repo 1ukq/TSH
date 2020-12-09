e04469250fd1a09c3a6d1e2e2ec0358c9140e301
Luka <luka.laval@etu.univ-paris-diderot.fr>
2020-12-09 14:47:12 +0100

    modifie date for makedir tar -tvf in Alpine

--- src/tests/script_bis.sh
+++ src/tests/script_bis.sh
@@ -56,11 +56,11 @@ mkdir test_dir_mkdir
 touch test_dir_mkdir/res_expected
 touch test_dir_mkdir/list_tar.sh
 
-echo -n "drwxr-xr-x $(id -u -n)/$(id -g -n)         0 $(date +%T) $(date +%R) test_dir/
--rw-r--r-- $(id -u -n)/$(id -g -n)        13 $(date +%T) $(date +%R) test_dir/helloworld
--rw-r--r-- $(id -u -n)/$(id -g -n)        19 $(date +%T) $(date +%R) test_dir/foo
--rw-r--r-- $(id -u -n)/$(id -g -n)      2090 $(date +%T) $(date +%R) bar
-drwxr-xr-x $(id -u -n)/$(id -g -n)         0 $(date +%T) $(date +%R) test_dir/makedir_test/
+echo -n "drwxr-xr-x $(id -u -n)/$(id -g -n)              0 $(date +%F) $(date +%T) test_dir/
+-rw-r--r-- $(id -u -n)/$(id -g -n)             13 $(date +%F) $(date +%T) test_dir/helloworld
+-rw-r--r-- $(id -u -n)/$(id -g -n)             19 $(date +%F) $(date +%T) test_dir/foo
+-rw-r--r-- $(id -u -n)/$(id -g -n)           2090 $(date +%F) $(date +%T) bar
+drwxr-xr-x $(id -u -n)/$(id -g -n)              0 $(date +%F) $(date +%T) test_dir/makedir_test/
 " > test_dir_mkdir/res_expected
 
 echo -n "#!/bin/bash
