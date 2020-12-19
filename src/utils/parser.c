7f2cf8b34211b1d73fcd2b451ef11661470b413e
Luka <luka.laval@etu.univ-paris-diderot.fr>
2020-12-19 21:47:06 +0100

    parser fonctionnel manque chemin abs

--- src/utils/parser.c
+++ src/utils/parser.c
@@ -1,29 +1,29 @@
 #include "parser.h"
 
 int count_cmd(char * string){
-	int i = 0;
-	char * string_copy = strdup(string);
-	char * token = strtok(string_copy, "|");
+	int i = 0, nb_cmd = 0;
 
-	while(token != NULL){
+	while(string[i] != '\0'){
+		if(string[i] == '|'){
+			nb_cmd++;
+		}
 		i++;
-		token = strtok(NULL, "|");
 	}
-	free(string_copy);
-	return i;
+
+	return nb_cmd + 1;
 }
 
 int count_words(char * string){
-	int i = 0;
-	char * string_copy = strdup(string);
-	char * token = strtok(string_copy, " ");
+	int i = 0, nb_words = 0;
 
-	while(token != NULL){
+	while(string[i] != '\0'){
+		if(string[i] == ' '){
+			nb_words++;
+		}
 		i++;
-		token = strtok(NULL, " ");
 	}
-	free(string_copy);
-	return i;
+
+	return nb_words;
 }
 
 
@@ -31,39 +31,42 @@ char *** parser(char * input){
 	int i = 0, j = 0;
 	// compter le nombre de commandes à stocker
 	int nb_cmd = count_cmd(input);
-	int nb_words;
 
 	// création du tableau à renvoyer
 	char *** tab = malloc(sizeof(char **) * nb_cmd);
 
-	// split cmd&args
+	// split input into an array of cmd
+	char * tab_cmd[nb_cmd];
+
 	char * input_copy = strdup(input);
-	char * split = strtok(input_copy, "|");
-	j = 0;
-
-	while(split != NULL){
-		// regader si split implique tar si oui rendre le chemin absolu
-		nb_words = count_words(split);
-
-		char * split_copy = strdup(split);
-		char * token = strtok(split_copy, " ");
-
-		tab[i] = malloc(sizeof(char *) * (nb_words+1));
-
-		while(token != NULL){
-			tab[i][j] = malloc(1);
-			sprintf(tab[i][j], "%s", token);
-			j++;
-			printf("%s %i\n",token ,*token);
-			token = strtok(NULL, " ");
-		}
-
-		printf("%s %i\n", split, *split);
+	char * token = strtok(input_copy, "|");
+	i = 0;
+	while(token != NULL){
+		tab_cmd[i] = strdup(token);
 		i++;
-		split = strtok(NULL, "|");
-		free(split_copy);
+		token = strtok(NULL, "|");
 	}
 	free(input_copy);
+
+	// split cmd into words and put them in tab structure
+	for(i = 0; i < nb_cmd; i++){
+
+		j = 0;
+		tab[i] = malloc(sizeof(char *) * count_words(tab_cmd[i]) + 1);
+
+		char * cmd_copy = strdup(tab_cmd[i]);
+		token = strtok(cmd_copy, " ");
+
+		while(token != NULL){
+			//rendre les chemins absolu
+			tab[i][j] = strdup(token);
+			j++;
+			token = strtok(NULL, " ");
+		}
+		tab[i][j] = NULL;
+		free(cmd_copy);
+	}
+	tab[i] = NULL;
 
 	return tab;
 }
@@ -73,17 +76,21 @@ int main(void){
 	char *** tab = parser("ls -l yo | head | mkdir prout");
 	int i = 0, j;
 
-	printf("\n\n\n");
+	printf("[ ");
 	while(tab[i] != NULL){
 		j = 0;
+		printf("[ ");
 		while(tab[i][j] != NULL){
-			printf("%s %i %i\n", tab[i][j], i, j);
+			printf("%s ", tab[i][j]);
 			j++;
 		}
+		printf("] ");
 		i++;
 	}
+	printf("]");
+	printf("\n");
 
-	free(tab);
+	//free(tab);
 
 	return 0;
 }
--- src/utils/parser.h
+++ src/utils/parser.h
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "fonctions_utiles.c"
 
 
 int count_cmd(char * string);
+
+int count_words(char * string);
 
 char *** parser(char * input);
