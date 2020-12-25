c4ec37a6667e32053b03bf42851be5c7738c5f67
Alessio Roveri <alessioroveri10@gmaail.com>
2020-12-25 22:14:13 +0100

    Correction des messages d'erreurs

--- src/removeDir.c
+++ src/removeDir.c
@@ -4,17 +4,17 @@
 int removeDirectory(const char *path_tar, const char *path_file_source){
 
   int fd_tar = open(path_tar, O_RDWR);
-  if(check_sys_call(fd_tar, "open in remove_file_from_tar") == -1) return -2;
+  if(check_sys_call(fd_tar, "open in removeDirectory") == -1) return -2;
 
   int size_tar = lseek(fd_tar, 0, SEEK_END);
-  if(check_sys_call(size_tar, "lseek in remove_file_from_tar") == -1) return -1;
+  if(check_sys_call(size_tar, "lseek in removeDirectory") == -1) return -1;
 
   int pos = lseek(fd_tar, 0, SEEK_SET);
-  if(check_sys_call(pos, "lseek in remove_file_from_tar") == -1) return -1;
+  if(check_sys_call(pos, "lseek in removeDirectory") == -1) return -1;
 
   struct posix_header header;
   ssize_t size_read = read(fd_tar, &header, BLOCK_SIZE);
-  if(check_sys_call(size_read, "read in remove_file_from_tar") == -1) return -1;
+  if(check_sys_call(size_read, "read in removeDirectory") == -1) return -1;
 
   int count = 0;
   int pos_dir = 0;
@@ -36,10 +36,10 @@ int removeDirectory(const char *path_tar, const char *path_file_source){
     //le shift pour passer au bloc suivant
     shift = file_size % BLOCK_SIZE == 0 ? file_size / BLOCK_SIZE : (file_size / BLOCK_SIZE) + 1;
     pos = lseek(fd_tar, shift * BLOCK_SIZE, SEEK_CUR);
-    if(check_sys_call(pos, "lseek in remove_file_from_tar") == -1) return -1;
+    if(check_sys_call(pos, "lseek in removeDirectory") == -1) return -1;
     //puis je lis le header pour la prochaine itération du while
     size_read = read(fd_tar, &header, BLOCK_SIZE);
-    if(check_sys_call(size_read, "read in remove_file_from_tar") == -1) return -1;
+    if(check_sys_call(size_read, "read in removeDirectory") == -1) return -1;
   }
   //Si il y a exactement une occurence du dossier
   if(count != 0){
@@ -49,15 +49,15 @@ int removeDirectory(const char *path_tar, const char *path_file_source){
 
     //Je met la tête de lecture après le directory qu'on veut supprimer, et je lis tout le contenu.
     pos = lseek(fd_tar, pos_dir+ BLOCK_SIZE, SEEK_SET);
-    if(check_sys_call(pos, "lseek in remove_file_from_tar") == -1) return -1;
+    if(check_sys_call(pos, "lseek in removeDirectory") == -1) return -1;
     size_read = read(fd_tar, buf_write, (size_tar - pos_dir - BLOCK_SIZE));
     if(check_sys_call(size_read, "read in remove_fichemin de racin jusqu'au tarle_from_tar") == -1) return -1;
     //Je met la tête de lecture au début du directory à supprimer afin de le supprimer
     pos = lseek(fd_tar, pos_dir, SEEK_SET);
-    if(check_sys_call(pos, "lseek in remove_file_from_tar") == -1) return -1;
+    if(check_sys_call(pos, "lseek in removeDirectory") == -1) return -1;
 
     int size_write = write(fd_tar, buf_write, (size_tar - pos_dir - BLOCK_SIZE));
-    if(check_sys_call(size_write, "write in remove_file_from_tar") == -1) return -1;
+    if(check_sys_call(size_write, "write in removeDirectory") == -1) return -1;
 
     close(fd_tar);
     free(buf_write);
