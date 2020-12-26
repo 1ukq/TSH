628d7573a4bc0d57221ebdcdab9e5f15e2e9afdc
KETELS Lucas <lucas.ketels@gmail.com>
2020-12-26 18:59:39 +0100

    Change location of some files

---
+++ src/usr_input_handler/tshell.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include "parser.h"
+#include "utils_rdr.h"
+#include "cmds_launcher.h"
+#include "../cd.h"
+
+#define PATH_MAX 4096
+
+int read_input(char * input);
+
+int tshell(void);
