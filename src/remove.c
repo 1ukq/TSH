5665aa57c29d42d9e8f6dd5c223bc79d757fcd90
KETELS Lucas <lucas.ketels@etu.univ-paris-diderot.fr>
2020-12-29 12:28:32 +0100

    Change rm function

--- src/remove.c
+++ src/remove.c
@@ -1,68 +1,17 @@
 #include "remove.h"
 
-
 int remove_file_from_tar(const char *path_tar, const char *path_file_source){
 
     int fd_tar = open(path_tar, O_RDWR);
     if(check_sys_call(fd_tar, "open in remove_file_from_tar") == -1) return -2;
-
+    int pos[3];
+    pos_file_in_tar(fd_tar, path_file_source, pos);
+    int pos_from = pos[0] - BLOCK_SIZE;
+    int pos_to = pos[1];
     int size_tar = lseek(fd_tar, 0, SEEK_END);
     if(check_sys_call(size_tar, "lseek in remove_file_from_tar") == -1) return -1;
-
-    int pos = lseek(fd_tar, 0, SEEK_SET);
-    if(check_sys_call(pos, "lseek in remove_file_from_tar") == -1) return -1;
-
-    struct posix_header header;
-    ssize_t size_read = read(fd_tar, &header, BLOCK_SIZE);
-    if(check_sys_call(size_read, "read in remove_file_from_tar") == -1) return -1;
-
-    int file_size = 0;
-    int shift = 0;
-
-    while(strcmp(header.name, path_file_source)){
-
-        if(header.name[0] == '\0'){
-            return -2;
-        }
-
-        sscanf(header.size, "%o", &file_size);
-        shift = file_size % BLOCK_SIZE == 0 ? file_size / BLOCK_SIZE : (file_size / BLOCK_SIZE) + 1;
-        pos = lseek(fd_tar, shift * BLOCK_SIZE, SEEK_CUR);
-        if(check_sys_call(pos, "lseek in remove_file_from_tar") == -1) return -1;
-
-        size_read = read(fd_tar, &header, BLOCK_SIZE);
-        if(check_sys_call(size_read, "read in remove_file_from_tar") == -1) return -1;
-    }
-
-    sscanf(header.size, "%o", &file_size);
-    shift = file_size % BLOCK_SIZE == 0 ? file_size / BLOCK_SIZE : (file_size / BLOCK_SIZE) + 1;
-
-    pos = lseek(fd_tar, -BLOCK_SIZE, SEEK_CUR);
-    if(check_sys_call(pos, "lseek in remove_file_from_tar") == -1) return -1;
-
-    char *buf_write = malloc(sizeof(char) * (size_tar - pos));
-    memset(buf_write, '\0', sizeof(char) * (size_tar - pos));
-
-    int n = pos;
-
-    pos = lseek(fd_tar, BLOCK_SIZE * (shift + 1), SEEK_CUR);
-    if(check_sys_call(pos, "lseek in remove_file_from_tar") == -1) return -1;
-
-    size_read = read(fd_tar, buf_write, (size_tar - n));
-    if(check_sys_call(size_read, "read in remove_file_from_tar") == -1) return -1;
-
-
-    pos = lseek(fd_tar, n, SEEK_SET);
-    if(check_sys_call(pos, "lseek in remove_file_from_tar") == -1) return -1;
-
-    int size_write = write(fd_tar, buf_write, size_tar - n);
-    if(check_sys_call(size_write, "write in remove_file_from_tar") == -1) return -1;
-
-    close(fd_tar);
-    free(buf_write);
-
+    suppress_file(fd_tar, pos_from, pos_to, size_tar);
     return 0;
-
 }
 
 int remove_file_from_tar_r(const char *path_tar, const char *path_dir){
